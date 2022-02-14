f5fe28228c5a1da88158763503a940164b3b7dc7
Ivan <ivan.ipv3@gmail.com>
2022-02-14 20:42:45 +0100

    Version 1.0

--- src/main/java/ivan/perviv/Main_selector.java
+++ src/main/java/ivan/perviv/Main_selector.java
@@ -4,6 +4,8 @@ import com.sun.tools.javac.Main;
 import java.awt.*;
 import java.io.*;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 /**
@@ -14,20 +16,47 @@ public class Main_selector extends JFrame {
 
     protected Image imagenIconito;
     protected File archivoPreguntas;
+    protected boolean tema;
 
     public Main_selector(String titulo) {
         super(titulo);
+        tema = false;
         initComponents();
         imagenIconito = getIconImage();
         setLocationRelativeTo(null);
         getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
     }
 
+    /**
+     * Metodo que carga una imagen para establecerla como icono del programa.
+     *
+     * @return imagen del icono del programa.
+     */
     @Override
     public Image getIconImage() {
         URL imageResource = Main.class.getClassLoader().getResource("Main_selector/iconito.png");
         Image iconito = Toolkit.getDefaultToolkit().getImage(imageResource);
         return iconito;
+    }
+
+    public void modoOscurito() {
+        tema = !tema;
+
+        if (tema) {
+            try {
+                UIManager.setLookAndFeel(new com.formdev.flatlaf.FlatLightLaf());
+            } catch (UnsupportedLookAndFeelException ex) {
+                Logger.getLogger(Main_selector.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            try {
+                UIManager.setLookAndFeel(new com.formdev.flatlaf.FlatDarkLaf());
+            } catch (UnsupportedLookAndFeelException ex) {
+                Logger.getLogger(Main_selector.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        SwingUtilities.updateComponentTreeUI(this);
+
     }
 
     @SuppressWarnings("unchecked")
@@ -38,10 +67,10 @@ public class Main_selector extends JFrame {
         Titulo = new javax.swing.JLabel();
         botonSelector = new javax.swing.JButton();
         botonEmpezar = new javax.swing.JButton();
-        botonAyuda = new javax.swing.JButton();
         jIcon = new javax.swing.JLabel();
         jIcon2 = new javax.swing.JLabel();
         logo = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setIconImage(getIconImage());
@@ -77,16 +106,24 @@ public class Main_selector extends JFrame {
             }
         });
 
-        botonAyuda.setText("?");
-        botonAyuda.setToolTipText("Ayuda.");
-        botonAyuda.putClientProperty("JButton.buttonType", "help");
-
         jIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Main_selector/abrir.png"))); // NOI18N
 
         jIcon2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Main_selector/play.png"))); // NOI18N
 
         logo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Main_selector/logo.png"))); // NOI18N
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Main_selector/modo-oscuro.png"))); // NOI18N
+        jButton1.setToolTipText("Cambiar tema.");
+        jButton1.setBorder(null);
+        jButton1.setBorderPainted(false);
+        jButton1.setContentAreaFilled(false);
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -110,11 +147,12 @@ public class Main_selector extends JFrame {
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(logo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(Titulo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(botonAyuda)))
+                            .addComponent(Titulo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -123,7 +161,7 @@ public class Main_selector extends JFrame {
                 .addComponent(logo, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(Titulo)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 83, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 67, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jIcon)
                     .addComponent(botonSelector, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -131,8 +169,8 @@ public class Main_selector extends JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jIcon2)
                     .addComponent(botonEmpezar, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(60, 60, 60)
-                .addComponent(botonAyuda)
+                .addGap(68, 68, 68)
+                .addComponent(jButton1)
                 .addContainerGap())
         );
 
@@ -172,31 +210,30 @@ public class Main_selector extends JFrame {
         }
     }//GEN-LAST:event_formWindowOpened
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // BOTON TEMA
+        modoOscurito();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        try {
-            //UIManager.setLookAndFeel(new com.formdev.flatlaf.FlatDarkLaf());
-            UIManager.setLookAndFeel(new com.formdev.flatlaf.FlatLightLaf());
-        } catch (Exception ex) {
-            java.util.logging.Logger.getLogger(Main_selector.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Main_selector("IP").setVisible(true);
+                Main_selector app = new Main_selector("IP");
+                app.modoOscurito();
+                app.setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Titulo;
-    private javax.swing.JButton botonAyuda;
     private javax.swing.JButton botonEmpezar;
     private javax.swing.JButton botonSelector;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jIcon;
     private javax.swing.JLabel jIcon2;
     private javax.swing.JSeparator jSeparator1;
--- src/main/java/ivan/perviv/Panel_resultados.java
+++ src/main/java/ivan/perviv/Panel_resultados.java
@@ -29,28 +29,31 @@ public class Panel_resultados extends JFrame {
         setLocationRelativeTo(null);
         getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
 
+        //Metodo en el cual pone en el label correspondiende (con una IMAGEN) + texto, si aprobado o suspendido.
         resultado(numeroDeAciertos,numeroDePreguntas);
         
-        jLabel2.setText(numeroDeAciertos + "/" + cantidadPreguntas);
+        //Pone en label el numero total de aciertos.
+        jAciertos.setText(numeroDeAciertos + "/" + cantidadPreguntas);
         for (int i = 0; i < listaDePreguntas.size(); i++) {
             jTabbedPane1.addTab(Integer.toString(i + 1), new ComponenteRespuestaFinal(listaDePreguntas.get(i)));
         }
     }
 
     /**
-     * Calcula si has aprovado o suspendido el examen.
+     * Calcula si has aprobado o suspendido el examen.
      * @param aciertos numero total de aciertos coseguidos.
      * @param preguntas numero de preguntas
      */
     public void resultado (int aciertos, int cantidadPreguntas){
         if (aciertos >=(cantidadPreguntas - 3)) {
-            jLabel1.setText("¡APROBADO!");
+            jResultado.setText("¡APROBADO!");
             imagenAprobado();
         } else {
-            jLabel1.setText("¡SUSPENSO!");
+            jResultado.setText("¡SUSPENSO!");
             imagenSuspenso();
         }
     }
+    
     /*
     * Inserta una imagen en el Label en el cual varia si apruebas o suspendes
      */
@@ -69,13 +72,14 @@ public class Panel_resultados extends JFrame {
     private void initComponents() {
 
         tituloFinal = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        jResultado = new javax.swing.JLabel();
+        jAciertos = new javax.swing.JLabel();
         imagenAprobadoSuspenso = new javax.swing.JLabel();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         respuestas = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jSeparator1 = new javax.swing.JSeparator();
+        version1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setIconImage(imagenIconito);
@@ -86,13 +90,13 @@ public class Panel_resultados extends JFrame {
         tituloFinal.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         tituloFinal.setText("¡HAS FINALIZADO!");
 
-        jLabel1.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("<RESULTADO>");
+        jResultado.setFont(new java.awt.Font("Rockwell", 0, 18)); // NOI18N
+        jResultado.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jResultado.setText("<RESULTADO>");
 
-        jLabel2.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("<ACIERTOS>");
+        jAciertos.setFont(new java.awt.Font("Rockwell", 0, 18)); // NOI18N
+        jAciertos.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jAciertos.setText("<ACIERTOS>");
 
         imagenAprobadoSuspenso.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 
@@ -105,13 +109,16 @@ public class Panel_resultados extends JFrame {
         respuestas.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         respuestas.setText("- RESPUESTAS CORRECTAS -");
 
-        jButton1.setText("MENU PRINCIPAL");
+        jButton1.setText("Menú Principal");
         jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+
+        version1.setFont(new java.awt.Font("Rockwell", 0, 12)); // NOI18N
+        version1.setText("Copyright © 2022");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -120,24 +127,28 @@ public class Panel_resultados extends JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 633, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addComponent(imagenAprobadoSuspenso, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGap(25, 25, 25)
+                    .addComponent(tituloFinal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jSeparator1)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(respuestas, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE))))
-                    .addComponent(tituloFinal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addComponent(jResultado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jAciertos, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(imagenAprobadoSuspenso, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(25, 25, 25)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(respuestas, javax.swing.GroupLayout.DEFAULT_SIZE, 560, Short.MAX_VALUE))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(214, 214, 214))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(44, 44, 44)
+                        .addComponent(version1)
+                        .addGap(35, 35, 35))
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(275, 275, 275))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -151,34 +162,38 @@ public class Panel_resultados extends JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(imagenAprobadoSuspenso, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel1)
+                        .addComponent(jResultado)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel2))
+                        .addComponent(jAciertos))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(respuestas, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 86, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30))
+                        .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 79, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(version1)
+                .addGap(25, 25, 25))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // BOTON PARA VOLVER A MENU DE INICIO //
         new Main_selector(titulo).setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel imagenAprobadoSuspenso;
+    private javax.swing.JLabel jAciertos;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jResultado;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JLabel respuestas;
     private javax.swing.JLabel tituloFinal;
+    private javax.swing.JLabel version1;
     // End of variables declaration//GEN-END:variables
 }
